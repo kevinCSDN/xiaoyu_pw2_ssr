f4ad3a4440cfa35e2cc0308a05caab7d50a9a415
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-10 21:20:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,46 @@
-sed -i '$a src-git small1 https://github.com/kenzok8/small' feeds.conf.default
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#删除feeds自带mosdns、v2ray-geodata
+rm -rf ./feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/themes/luci-theme-argon
+
+
+
+
+
+
+
+
+#克隆passwall环境插件 单独克隆插件有助于成功，前面拉取kenzo的small库作为依赖就没编译成功
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+
+#克隆的源码放在small文件夹,预先建立small文件夹
+mkdir package/small
+pushd package/small
+
+#克隆源码
+
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+#git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+#mosdns
+#git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+#git clone --depth=1 https://github.com/fw876/helloworld.git
+#git clone https://github.com/kevinCSDN/lua-maxminddb.git  #git lua-maxminddb 依赖
+
+#git clone https://github.com/kevinCSDN/luci-app-vssr-plus.git  
+
+popd
